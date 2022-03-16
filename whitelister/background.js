bff43aecc885b2ac29090f8e03cb5c3c1efb6dfe
joshm21 <44173063+joshm21@users.noreply.github.com>
2022-03-16 00:35:22 -0500

    Update background.js

--- whitelister/background.js
+++ whitelister/background.js
@@ -1,29 +1,31 @@
-const SERVER_ENDPOINT = "your_server_endpoint_url"
-const MINUTES_UNTIL_RECHECK_INCOGNITO = 0.1
+const SERVER_ENDPOINT = ""
+const MINUTES_UNTIL_NEXT_ALARM = 0.1
 
+
+
+const checkIncognitoAccessAllowed = () => {
+  chrome.extension.isAllowedIncognitoAccess(isAllowedAccess => {
+    if (!isAllowedAccess) {
+      postToServer({ event: "incognitoAccessDisallowed" })
+    }
+  })
+}
 
 const onOtherExtensionDisabledOrUninstalled = async (extensionInfo) => {
-  if (extensionInfo.name == await getPartnerExtensionName()) {
-    postToServer("disabledOrUninstalled")
+  const thisName = await getExtensionName()
+  const otherName = extensionInfo.name
+  if (`${thisName} Guard` == otherName || thisName == `${otherName} Guard`) {
+    postToServer({ event: "partnerExtensionDisabledOrUninstalled" })
   }
 }
 
-const checkIncognitoAccessAllowed = async (alarm) => {
-  chrome.alarms.create({ delayInMinutes: MINUTES_UNTIL_RECHECK_INCOGNITO })
-  if (!(await isAllowedIncognitoAccess())) {
-    postToServer("incognitoAccessDisallowed")
-  }
-}
-
-const postToServer = async (event) => {
-  const userEmail = await getUserEmail()
+const postToServer = async (payload) => {
+  payload.timestamp = new Date().toISOString()
+  payload.userEmail = await getUserEmail()
+  payload.source = await getExtensionName()
   fetch(SERVER_ENDPOINT, {
     method: "POST",
-    body: JSON.stringify({
-      timestamp: new Date().toISOString(),
-      userEmail,
-      event
-    }),
+    body: JSON.stringify({ payload }),
     headers: { "Content-Type": "text/plain;charset=utf-8" }
   })
 }
@@ -37,20 +39,11 @@ const getUserEmail = async () => {
   })
 }
 
-const isAllowedIncognitoAccess = async () => {
+const getExtensionName = async () => {
   return new Promise(resolve => {
-    chrome.extension.isAllowedIncognitoAccess(isAllowedAccess => resolve(isAllowedAccess))
+    chrome.management.getSelf(extensionInfo => resolve(extensionInfo.name))
   })
 }
-
-const getPartnerExtensionName = () => {
-  return new Promise(resolve => {
-    chrome.management.getSelf(extensionInfo => {
-      extensionInfo.name == "Whitelister" ? resolve("Whitelister Guard") : resolve("Whitelister")
-    })
-  })
-}
-
 
 
 
